596c43933eb33c7e15342d7fee09af94c33c9949
Ulrich Pfeifer <pfeifer@wait.de>
1998-01-13 03:12:15 -0800

    import CPAN-Site 0.008 from CPAN
    git-cpan-module:   CPAN-Site
    git-cpan-version:  0.008
    git-cpan-authorid: ULPFR
    git-cpan-file:     authors/id/U/UL/ULPFR/CPAN-Site-0.008.tar.gz

--- Site.pm
+++ Site.pm
@@ -1,12 +1,12 @@
 #                              -*- Mode: Perl -*- 
 # $Basename: Site.pm $
-# $Revision: 1.8 $
+# $Revision: 1.9 $
 # Author          : Ulrich Pfeifer
 # Created On      : Wed Jan  7 11:42:46 1998
 # Last Modified By: Ulrich Pfeifer
-# Last Modified On: Thu Jan  8 12:57:40 1998
+# Last Modified On: Tue Jan 13 11:58:10 1998
 # Language        : CPerl
-# Update Count    : 10
+# Update Count    : 12
 # Status          : Unknown, Use with caution!
 # 
 # (C) Copyright 1998, Ulrich Pfeifer, all rights reserved.
@@ -19,7 +19,7 @@ use vars qw($VERSION @ISA);
 @ISA = qw(CPAN);
 
 # $Format: "$VERSION = sprintf '%5.3f', ($ProjectMajorVersion$ * 100 + ($ProjectMinorVersion$-1))/1000;"$
-$VERSION = sprintf '%5.3f', (0 * 100 + (8-1))/1000;
+$VERSION = sprintf '%5.3f', (0 * 100 + (9-1))/1000;
 
 # This line is edited by Makefile.PL. Don't change formatting etc.
 unshift @{$CPAN::Config->{urllist}}, q[file:/tmp/CPAN/]
